c56ddf39753cc119d747eb7d03dc93f34e56878d
Charles Pereira <charlesti.ptu@gmail.com>
2023-02-26 20:44:39 -0300

    component historycard and sectionlist

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Center, Heading, HStack, Text, VStack } from 'native-base';
+
+type HistoryCardProps = { title?: string };
+
+export const HistoryCard: React.FC<HistoryCardProps> = ({ title }) => {
+  return (
+    <HStack
+      w="full"
+      px={5}
+      paddingY={4}
+      marginBottom={3}
+      bg="gray.600"
+      rounded="md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack>
+        <Heading color="white" fontSize="md" textTransform="capitalize">
+          Costas
+        </Heading>
+
+        <Text color="gray.100" fontSize="lg" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
+
+      <Text color="gray.300" fontSize="md">
+        08:23
+      </Text>
+    </HStack>
+  );
+};
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import { Center, Text, VStack } from 'native-base';
+import React, { useState } from 'react';
+import { Heading, VStack, SectionList } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 type HistoryProps = {};
 
 export const History: React.FC<HistoryProps> = ({}) => {
+  const [exercises, setExercises] = useState([
+    {
+      title: '27/08/2023',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '28/08/2023',
+      data: ['Puxada frontal'],
+    },
+  ]);
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   );
 };
