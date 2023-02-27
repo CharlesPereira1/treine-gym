c5f977f0f0d7010db71ea7931ffb59c2dbcdc2b5
Charles Pereira <charlesti.ptu@gmail.com>
2023-02-26 21:12:37 -0300

    style profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
-import { ScrollView, VStack, Center, Skeleton } from 'native-base';
+import {
+  ScrollView,
+  VStack,
+  Center,
+  Skeleton,
+  Text,
+  Heading,
+} from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
+import { TouchableOpacity } from 'react-native';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 type ProfileProps = {};
 
@@ -30,7 +40,40 @@ export const Profile: React.FC<ProfileProps> = ({}) => {
               size={PHOTO_SIZE}
             />
           )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input bg="gray.600" placeholder="Nome" />
+          <Input bg="gray.600" value="charlesti.ptu@gmail.com" isDisabled />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirme nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
