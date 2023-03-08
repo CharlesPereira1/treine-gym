dc3ea32ff163fedf8ca99de28c1049442ddcd52a
Charles Pereira <charlesti.ptu@gmail.com>
2023-03-07 22:55:00 -0300

    axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import axios from 'axios';
 
 import { AuthNavigationroutesProps } from '@routes/auth.routes';
 
@@ -53,11 +55,17 @@ export const SignUp: React.FC<SignUpProps> = ({}) => {
   };
 
   const handleSignUp = async ({ name, email, password }: FormDataProps) => {
-    await api.post('users', {
-      name,
-      email,
-      password,
-    });
+    try {
+      await api.post('users', {
+        name,
+        email,
+        password,
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
   };
 
   return (
